1f743495cbe8b7f279c70c4f80e57b63ae3d6f1b
LittleFlyFish <buliyanran@gmail.com>
2020-01-22 19:03:23 +0000

    "Dense_GCN add"

---
+++ engineer/models/backbones/Dense_GCN.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import print_function
+
+import torch.nn as nn
+import torch
+from torch.nn.parameter import Parameter
+import math
+from engineer.models.registry import BACKBONES
+from engineer.models.backbones.Motion_GCN import GraphConvolution, GC_Block
+import numpy as np
+
+class GC_Block_NoRes(nn.Module):
+    def __init__(self, in_features, p_dropout, bias=True, node_n=48):
+        """
+        Define a residual block of GCN
+        """
+        super(GC_Block, self).__init__()
+        self.in_features = in_features
+        self.out_features = in_features
+
+        self.gc1 = GraphConvolution(in_features, in_features, node_n=node_n, bias=bias)
+        self.bn1 = nn.BatchNorm1d(node_n * in_features)
+
+        self.gc2 = GraphConvolution(in_features, in_features, node_n=node_n, bias=bias)
+        self.bn2 = nn.BatchNorm1d(node_n * in_features)
+
+        self.do = nn.Dropout(p_dropout)
+        self.act_f = nn.Tanh()
+
+    def forward(self, x):
+        y = self.gc1(x)
+        b, n, f = y.shape
+        y = self.bn1(y.view(b, -1)).view(b, n, f)
+        y = self.act_f(y)
+        y = self.do(y)
+
+        y = self.gc2(y)
+        b, n, f = y.shape
+        y = self.bn2(y.view(b, -1)).view(b, n, f)
+        y = self.act_f(y)
+        y = self.do(y)
+
+        return y
+
+    def __repr__(self):
+        return self.__class__.__name__ + ' (' \
+               + str(self.in_features) + ' -> ' \
+               + str(self.out_features) + ')'
+
+@BACKBONES.register_module
+class Dense_GCN(nn.Module):
+    '''
+    Original Module GCN structure
+    '''
+    def __init__(self, input_feature, hidden_feature, p_dropout, num_stage=1, node_n=48):
+        """
+
+        :param input_feature: num of input feature
+        :param hidden_feature: num of hidden feature
+        :param p_dropout: drop out prob.
+        :param num_stage: number of residual blocks
+        :param node_n: number of nodes in graph
+        """
+        super(Dense_GCN, self).__init__()
+        self.num_stage = num_stage
+
+        self.gc1 = GraphConvolution(input_feature, hidden_feature, node_n=node_n)
+        self.bn1 = nn.BatchNorm1d(node_n * hidden_feature)
+
+        self.gcbs = []
+        for i in range(num_stage):
+            self.gcbs.append(GC_Block_NoRes(hidden_feature, p_dropout=p_dropout, node_n=node_n))
+
+        self.gcbs = nn.ModuleList(self.gcbs)
+
+        self.gc7 = GraphConvolution(hidden_feature, input_feature, node_n=node_n)
+
+        self.do = nn.Dropout(p_dropout)
+        self.act_f = nn.Tanh()
+
+
+        self.mlp = []
+        for i in range(num_stage):
+            self.mlp.append(nn.Linear((i+1)*15, (i+2)*15))
+
+    def forward(self, x):
+        y = self.gc1(x)
+        b, n, f = y.shape
+        y = self.bn1(y.view(b, -1)).view(b, n, f)
+        y = self.act_f(y)
+        y = self.do(y)
+
+        for i in range(self.num_stage):
+            print("here is the size")
+            print(y.shape)
+            y1 = self.gcbs[i](y) # y size [batch, node_n, dct_n]
+            y = np.concatenate((y, y1), axis=2)
+            print(y.shape)
+            y = self.mlp[i](y)
+            print(y.shape)
+
+
+        y = self.gc7(y)
+        y = y + x
+
+        return y
--- engineer/models/backbones/__init__.py
+++ engineer/models/backbones/__init__.py
@@ -7,5 +7,7 @@ from .Recycle_GCN import Recycle_GCN
 from .G_NoNorm import G_NoNorm
 from .G_Attention import G_Attention
 from .G_Attention_GCN import G_Attention_GCN
-__all__=["Motion_GCN","SemGCN","Cycle_GCN", "P_GCN", "Cycle_P", "Recycle_GCN", "G_NoNorm", "G_Attention", "G_Attention_GCN"]
+from .Dense_GCN import Dense_GCN
+__all__=["Motion_GCN","SemGCN","Cycle_GCN", "P_GCN", "Cycle_P", "Recycle_GCN", "G_NoNorm",
+         "G_Attention", "G_Attention_GCN", "Dense_GCN"]
 
