c36bbc1e94dc1e7f28c28f5445f62b66ae7c4564
LittleFlyFish <buliyanran@gmail.com>
2020-02-25 13:14:03 +0000

    "GV=G version, Padding=True"

--- engineer/core/train_seq2seq.py
+++ engineer/core/train_seq2seq.py
@@ -182,13 +182,13 @@ def train(train_loader, model, optimizer, lr_now=None, max_norm=True, is_cuda=Fa
             all_seq = Variable(all_seq.cuda(non_blocking=True)).float()
 
         outputs = model(inputs.transpose(0,1), targets.transpose(0,1)) # [10, batch, 198]
-        seg = torch.cat([inputs.transpose(0,1), outputs], dim=0) # [15, 16, 198]
+        seg = torch.cat([inputs.transpose(0,1), targets], dim=0) # [15, 16, 198]
 
         # calculate loss and backward
         outputs = seg2whole(seg, dct_n) # [16, 66, 15]
-        _, loss = loss_funcs.mpjpe_error_p3d_seq2seq(targets, all_seq, dct_n, dim_used)
+        _, loss = loss_funcs.mpjpe_error_p3d_seq2seq(outputs, all_seq, dct_n, dim_used)
         print(loss)
-        
+
         # lossM = nn.MSELoss()
         # loss = lossM(outputs.transpose(0,1), targets)
         loss = Variable(loss, requires_grad=True)
