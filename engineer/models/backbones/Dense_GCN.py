2aeb60d7f60e1345f532a45703dfb8ee9c05c773
LittleFlyFish <buliyanran@gmail.com>
2020-04-03 05:44:38 +0800

    "GV=G version, Padding=True"

--- engineer/models/backbones/Dense_GCN.py
+++ engineer/models/backbones/Dense_GCN.py
@@ -66,8 +66,12 @@ class Dense_GCN(nn.Module):
         super(Dense_GCN, self).__init__()
         self.num_stage = num_stage
 
-        self.gc1 = GraphConvolution(input_feature, hidden_feature, node_n=node_n)
+        self.gc1 = GraphConvolution(input_feature + 16 + 14 + 11, hidden_feature, node_n=node_n)
         self.bn1 = nn.BatchNorm1d(node_n * hidden_feature)
+        self.bn7 = nn.BatchNorm1d(node_n * input_feature)
+        self.bnc1 = nn.BatchNorm1d(node_n * 16)
+        self.bnc2 = nn.BatchNorm1d(node_n * 14)
+        self.bnc3 = nn.BatchNorm1d(node_n * 11)
 
         self.gcbs = []
         for i in range(num_stage):
@@ -77,12 +81,37 @@ class Dense_GCN(nn.Module):
 
         self.gc7 = GraphConvolution(hidden_feature*(num_stage+1), input_feature, node_n=node_n)
 
+        self.conv1 = nn.Conv1d(node_n, node_n, 5, stride=1)
+        self.conv2 = nn.Conv1d(node_n, node_n, 7, stride=1)
+        self.conv3 = nn.Conv1d(node_n, node_n, 10, stride=1)
+        self.Iconv1 = nn.ConvTranspose1d(node_n, node_n, 5, stride=1)
+
         self.do = nn.Dropout(p_dropout)
         self.act_f = nn.LeakyReLU()
+        self.residual = residual
 
+    def forward(self, x, seq):
+        y_c1 = self.conv1(seq.transpose(1, 2)) # [16, 66, 16]
+        b, n, f = y_c1.shape
+        y_c1 = self.bnc1(y_c1.view(b, -1)).view(b, n, f)
+        y_c1 = self.act_f(y_c1)
+        y_c1 = self.do(y_c1)
 
-    def forward(self, x):
-        y = self.gc1(x)
+        y_c2 = self.conv2(seq.transpose(1, 2)) # [16, 66, 14]
+        b, n, f = y_c2.shape
+        y_c2 = self.bnc2(y_c2.view(b, -1)).view(b, n, f)
+        y_c2 = self.act_f(y_c2)
+        y_c2 = self.do(y_c2)
+
+        y_c3 = self.conv3(seq.transpose(1, 2)) # [16, 66, 11]
+        b, n, f = y_c3.shape
+        y_c3 = self.bnc3(y_c3.view(b, -1)).view(b, n, f)
+        y_c3 = self.act_f(y_c3)
+        y_c3 = self.do(y_c3)
+
+        y = torch.cat((y_c1, y_c2, y_c3, x), dim=2)
+
+        y = self.gc1(y)
         b, n, f = y.shape
         y = self.bn1(y.view(b, -1)).view(b, n, f)
         y = self.act_f(y)
