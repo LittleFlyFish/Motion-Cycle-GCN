02a22bb070a2d2edd3ace3060bb988fbbc2c8081
LittleFlyFish <buliyanran@gmail.com>
2020-04-04 02:46:29 +0800

    "GV=G version, Padding=True"

--- scripts/March_30.sh
+++ scripts/March_30.sh
@@ -1,7 +1,6 @@
 python3 tools/train_smooth.py --config ./configs/Motion_GCN_I10_O10_D15_G_smooth.py
 python3 tools/train_conv.py --config ./configs/Motion_GCN_I10_O10_D15_G_conv.py
-python3 tools/train_conv.py --config ./configs/Motion_GCN_I10_O10_D15_G_conv3.py
-python3 tools/train_conv.py --config ./configs/Motion_GCN_I10_O10_D15_G_convRELU.py
+
 python3 tools/train_conv.py --config ./configs/Dense_GCN_I10_O10_D15_G.py
 
 
