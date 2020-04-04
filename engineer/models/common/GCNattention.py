bd59a45f592b2d428a1b1f9b5597691e77f1c7f7
LittleFlyFish <buliyanran@gmail.com>
2020-04-05 06:27:14 +0800

    "GV=G version, Padding=True"

--- engineer/models/common/GCNattention.py
+++ engineer/models/common/GCNattention.py
@@ -55,6 +55,32 @@ class GraphAttentionLayer(nn.Module):
     def __repr__(self):
         return self.__class__.__name__ + ' (' + str(self.in_features) + ' -> ' + str(self.out_features) + ')'
 
+class SpecialSpmmFunction(torch.autograd.Function):
+    """Special function for only sparse region backpropataion layer."""
+    @staticmethod
+    def forward(ctx, indices, values, shape, b):
+        assert indices.requires_grad == False
+        a = torch.sparse_coo_tensor(indices, values, shape)
+        ctx.save_for_backward(a, b)
+        ctx.N = shape[1]
+        return torch.matmul(a, b)
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        a, b = ctx.saved_tensors
+        grad_values = grad_b = None
+        if ctx.needs_input_grad[1]:
+            grad_a_dense = grad_output.matmul(b.t())
+            edge_idx = a._indices()[0, :] * ctx.N + a._indices()[1, :]
+            grad_values = grad_a_dense.view(-1)[edge_idx]
+        if ctx.needs_input_grad[3]:
+            grad_b = a.t().matmul(grad_output)
+        return None, grad_values, None, grad_b
+
+
+class SpecialSpmm(nn.Module):
+    def forward(self, indices, values, shape, b):
+        return SpecialSpmmFunction.apply(indices, values, shape, b)
 
 
 class SpGraphAttentionLayer(nn.Module):
@@ -79,48 +105,56 @@ class SpGraphAttentionLayer(nn.Module):
          self.leakyrelu = nn.LeakyReLU(self.alpha)
          self.special_spmm = SpecialSpmm()
 
-     def forward(self, input, adj):
-         dv = 'cuda' if input.is_cuda else 'cpu'
+     def forward(self, inputBatch, adj):
+         b = inputBatch.size[0]
+         inputList = torch.split(inputBatch,1,dim=0)
+         t = 0
+         ResultList = [None]*b
+         for input in inputList:
+             N = input.size()[0]  # 2708
+             edge = adj.nonzero().t()  # non zero edge
 
-         N = input.size()[0]
-         edge = adj.nonzero().t()
+             h = torch.mm(input, self.W)  # [2708, 8]
+             # h: N x out
+             assert not torch.isnan(h).any()
 
-         h = torch.mm(input, self.W)
-         # h: N x out
-         assert not torch.isnan(h).any()
+             # Self-attention on the nodes - Shared attention mechanism
+             edge_h = torch.cat((h[edge[0, :], :], h[edge[1, :], :]), dim=1).t()
+             # edge: 2*D x E
 
-         # Self-attention on the nodes - Shared attention mechanism
-         edge_h = torch.cat((h[edge[0, :], :], h[edge[1, :], :]), dim=1).t()
-         # edge: 2*D x E
+             edge_e = torch.exp(-self.leakyrelu(self.a.mm(edge_h).squeeze()))
+             assert not torch.isnan(edge_e).any()
+             # edge_e: E
 
-         edge_e = torch.exp(-self.leakyrelu(self.a.mm(edge_h).squeeze()))
-         assert not torch.isnan(edge_e).any()
-         # edge_e: E
+             e_rowsum = self.special_spmm(edge, edge_e, torch.Size([N, N]), torch.ones(size=(N, 1), device=dv))
+             # e_rowsum: N x 1
 
-         e_rowsum = self.special_spmm(edge, edge_e, torch.Size([N, N]), torch.ones(size=(N, 1), device=dv))
-         # e_rowsum: N x 1
+             edge_e = self.dropout(edge_e)
+             # edge_e: E
 
-         edge_e = self.dropout(edge_e)
-         # edge_e: E
+             h_prime = self.special_spmm(edge, edge_e, torch.Size([N, N]), h)  # [2708, 8]
+             assert not torch.isnan(h_prime).any()
+             # h_prime: N x out
 
-         h_prime = self.special_spmm(edge, edge_e, torch.Size([N, N]), h)
-         assert not torch.isnan(h_prime).any()
-         # h_prime: N x out
+             h_prime = h_prime.div(e_rowsum)
+             # h_prime: N x out
+             assert not torch.isnan(h_prime).any()
 
-         h_prime = h_prime.div(e_rowsum)
-         # h_prime: N x out
-         assert not torch.isnan(h_prime).any()
+             if self.concat:
+                 # if this layer is not last layer,
+                 results = F.elu(h_prime)
+             else:
+                 # if this layer is last layer,
+                 results = h_prime
+             ResultList[t] = results
+             t = t + 1
+         results = torch.cat(ResultList, dim=0)
+         return results
 
-         if self.concat:
-             # if this layer is not last layer,
-             return F.elu(h_prime)
-         else:
-             # if this layer is last layer,
-             return h_prime
+
 
      def __repr__(self):
          return self.__class__.__name__ + ' (' + str(self.in_features) + ' -> ' + str(self.out_features) + ')'
-
 
 class GAT(nn.Module):
     def __init__(self, nfeat, nhid, nclass, dropout, alpha, nheads):
