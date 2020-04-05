4766eed859fed2a44e26c448b617c7a222773d77
LittleFlyFish <buliyanran@gmail.com>
2020-04-05 08:32:27 +0800

    "nice"

--- configs/Motion_GCN_I10_O10_D15_G_conv.py
+++ configs/Motion_GCN_I10_O10_D15_G_conv.py
@@ -12,7 +12,7 @@ data_root = './engineer/datasets/h3.6m/dataset'
 train_pipeline = [
     dict(type='SampleFrames', direction = True),
 ]
-cuda_num ='cuda:1'
+cuda_num ='cuda:3'
 flag = 'Conv+L1'
 meta ='./checkpoints/_tempconfig/ckpt_train_conv_3D_in10_out10_dct_n_15Conv+L2_best.pth.tar'
 
