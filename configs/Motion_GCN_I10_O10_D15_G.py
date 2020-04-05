6c617c8d97f55c422042ecfa78878ed2cb52a797
LittleFlyFish <buliyanran@gmail.com>
2020-04-06 00:28:52 +0800

    "nice"

--- configs/Motion_GCN_I10_O10_D15_G.py
+++ configs/Motion_GCN_I10_O10_D15_G.py
@@ -12,7 +12,7 @@ data_root = './engineer/datasets/h3.6m/dataset'
 train_pipeline = [
     dict(type='SampleFrames',direction = True),
 ]
-cuda_num = 'cuda:2'
+cuda_num = 'cuda:0'
 flag = 'Original+L1'
 
 val_pipeline = [
