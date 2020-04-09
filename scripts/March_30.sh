4eb4fd1cd272d3677e9d8170e48b670c6a78294a
LittleFlyFish <buliyanran@gmail.com>
2020-04-10 06:43:21 +0800

    "nice"

--- scripts/March_30.sh
+++ scripts/March_30.sh
@@ -4,7 +4,7 @@ python3 tools/train_net.py --config ./configs/Motion_GCN_I10_O10_D15_G.py
 python3 tools/train_net.py --config ./configs/ST_GCNA_I10_O10_D15_G.py
 
 python3 tools/train_conv.py --config ./configs/Dense_GCN_I10_O10_D15_G.py
-python3 tools/train_NewPCycle.py --config ./configs/Motion_GCN_I10_O10_D15_G.py
+python3 tools/train_NewCycle.py --config ./configs/Motion_GCN_I10_O10_D15_G.py
 
 
 python3 tools/train_offsetloss.py --config ./configs/Motion_GCN_I10_O10_D15_G_offset.py
