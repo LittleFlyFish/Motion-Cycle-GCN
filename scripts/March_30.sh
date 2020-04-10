e809d6f0b617a38fc2d11c9db52d74e4a3dcb958
LittleFlyFish <buliyanran@gmail.com>
2020-04-10 12:06:19 +0800

    "nice"

--- scripts/March_30.sh
+++ scripts/March_30.sh
@@ -9,9 +9,9 @@ python3 tools/train_offsetloss.py --config ./configs/Motion_GCN_I10_O10_D15_G_of
 
 
 # April 10th experiments
-python3 tools/train_NewCycle.py --config ./configs/Motion_GCN_I10_O10_D15_G.py
+python3 tools/train_NewCycle.py --config ./configs/Motion_GCN_I10_O10_D15_G.py # window5, lamda=0.1
 python3 tools/train_NewPCycle.py --config ./configs/Motion_GCN_I10_O10_D15_G.py # window4, cuda1
 python3 tools/train_NewCycle.py --config ./configs/Dense_GCN_I10_O10_D15_G.py
 python3 tools/train_NewPCycle.py --config ./configs/Dense_GCN_I10_O10_D15_G.py # window3, cuda2
 
-python3 tools/train_net.py --config ./configs/Motion_GCN_I10_O10_D15_G.py #
+python3 tools/train_net.py --config ./configs/Motion_GCN_I10_O10_D15_G.py # window2, cuda0
