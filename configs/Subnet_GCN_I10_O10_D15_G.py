95733fb9619c97b0ff3f186f240691e121f0daba
LittleFlyFish <buliyanran@gmail.com>
2020-04-12 07:18:31 +0800

    "nice"

--- configs/Subnet_GCN_I10_O10_D15_G.py
+++ configs/Subnet_GCN_I10_O10_D15_G.py
@@ -13,7 +13,7 @@ train_pipeline = [
     dict(type='SampleFrames',direction = True),
 ]
 cuda_num = 'cuda:3'
-flag = 'Subnet_GCN_lr=0.001+LR+TwoLoss'
+flag = 'Subnet_GCN_lr=0.001+LR+TwoLoss+RELU'
 
 val_pipeline = [
     dict(type='SampleFrames', direction=True),
