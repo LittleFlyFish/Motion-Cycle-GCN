5d1e68f0f0e444e48fe96dd7bb3c473ab71bd0d2
LittleFlyFish <buliyanran@gmail.com>
2020-04-12 10:11:36 +0800

    "nice"

--- configs/Subnet_GCN_I10_O10_D15_G.py
+++ configs/Subnet_GCN_I10_O10_D15_G.py
@@ -2,7 +2,7 @@ import numpy as np
 model = dict(
     type='Subnet_GCN',
     input_feature=15,
-    hidden_feature=256,
+    hidden_feature=128,
     dropout=0.5,
     num_stage=12,
     node_n=66
@@ -13,7 +13,7 @@ train_pipeline = [
     dict(type='SampleFrames',direction = True),
 ]
 cuda_num = 'cuda:2'
-flag = 'Subnet_GCN_lr=0.001+LR+TwoLoss+RELU+V5'
+flag = 'Subnet_GCN_lr=0.001_f=128+LR+TwoLoss'
 
 val_pipeline = [
     dict(type='SampleFrames', direction=True),
