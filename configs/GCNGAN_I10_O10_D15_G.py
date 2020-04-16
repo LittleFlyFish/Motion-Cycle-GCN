bf9d6bccaf4c9995d0a3c16def62a70069f55515
LittleFlyFish <buliyanran@gmail.com>
2020-04-16 09:32:29 +0800

    "nice"

--- configs/GCNGAN_I10_O10_D15_G.py
+++ configs/GCNGAN_I10_O10_D15_G.py
@@ -30,7 +30,7 @@ data_root = './engineer/datasets/h3.6m/dataset'
 train_pipeline = [
     dict(type='SampleFrames',direction = True),
 ]
-cuda_num = 'cuda:1'
+cuda_num = 'cuda:0'
 flag = 'GCNGAN'
 
 val_pipeline = [
