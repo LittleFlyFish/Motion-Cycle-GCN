a486b38b1440678707cd3cd33b703c8f4572e1c5
LittleFlyFish <buliyanran@gmail.com>
2020-04-16 08:40:35 +0800

    "nice"

--- engineer/models/backbones/GCNGAN.py
+++ engineer/models/backbones/GCNGAN.py
@@ -25,156 +25,157 @@ from engineer.models.backbones.Motion_GCN import Motion_GCN, GraphConvolution, G
 
 
 
-@BACKBONES.register_module
-class Discriminator(nn.Module):
-    '''
-    Original Module GCN structure
-    '''
-    def __init__(self, input_feature, hidden_feature, p_dropout, num_stage=1, node_n=48, residual=True):
-        """
-        input = [batch, node, dct_n]
-        :param input_feature: num of input feature
-        :param hidden_feature: num of hidden feature
-        :param p_dropout: drop out prob.
-        :param num_stage: number of residual blocks
-        :param node_n: number of nodes in graph
-        """
-        super(Discriminator, self).__init__()
-        self.num_stage = num_stage
-
-        self.gc1 = GraphConvolution(input_feature, hidden_feature, node_n=node_n)
-        self.bn1 = nn.BatchNorm1d(node_n * hidden_feature)
-        self.bn7 = nn.BatchNorm1d(node_n * input_feature)
-
-        self.gcbs = []
-        for i in range(num_stage):
-            self.gcbs.append(GC_Block(hidden_feature, p_dropout=p_dropout, node_n=node_n))
-
-        self.gcbs = nn.ModuleList(self.gcbs)
-
-        self.gc7 = GraphConvolution(hidden_feature, input_feature, node_n=node_n)
-
-        self.do = nn.Dropout(p_dropout)
-        self.act = nn.Tanh()
-        self.residual = residual
-        self.Softmax = nn.Softmax()
-        self.fcn = nn.Linear(node_n*input_feature, 2)
-
-    def forward(self, x):
-        y = self.gc1(x)
-        b, n, f = y.shape
-        y = self.bn1(y.view(b, -1)).view(b, n, f)
-        y = self.act(y)
-        y = self.do(y)
-
-        for i in range(self.num_stage):
-            y = self.gcbs[i](y)
-
-        if self.residual == True:
-            y = self.gc7(y)
-            y = self.fcn(y.view(b, -1))
-            y = self.Softmax(y)
-        #else:
-            # y = self.gc7(y)
-            # b, n, f = y.shape
-            # y = self.bn7(y.view(b, -1)).view(b, n, f)
-            # y = self.act(y)
-            # y = self.do(y)
-        return y
 
 # class Discriminator(nn.Module):
 #     '''
-#     Use GCN as encoder, and then use gcn as a decoder
-#     The input is [batch, node_dim, dct_n]   # for example, [16, 66, 15]
-#     the out feature of encoder is  [batch, node_dim, feature_len], this is dct feature version.
+#     Original Module GCN structure
 #     '''
-#     def __init__(self,  hidden_feature, layout, strategy, dropout, residual, **kwargs):
+#     def __init__(self, input_feature, hidden_feature, p_dropout, num_stage=1, node_n=48, residual=True):
 #         """
-#
-#         :param input_feature: num of input feature, dct_n
+#         input = [batch, node, dct_n]
+#         :param input_feature: num of input feature
 #         :param hidden_feature: num of hidden feature
 #         :param p_dropout: drop out prob.
 #         :param num_stage: number of residual blocks
 #         :param node_n: number of nodes in graph
 #         """
 #         super(Discriminator, self).__init__()
-#         # load graph
-#         self.graph = Graph(layout=layout, strategy=strategy)
-#         A = torch.tensor(self.graph.A, dtype=torch.float32, requires_grad=False)
-#         self.register_buffer('A', A)
+#         self.num_stage = num_stage
 #
-#         self.graph_d1 = Graph(layout="h36m_d1", strategy=strategy)
-#         A_d1 = torch.tensor(self.graph_d1.A, dtype=torch.float32, requires_grad=False)
-#         self.register_buffer('A_d1', A_d1)
+#         self.gc1 = GraphConvolution(input_feature, hidden_feature, node_n=node_n)
+#         self.bn1 = nn.BatchNorm1d(node_n * hidden_feature)
+#         self.bn7 = nn.BatchNorm1d(node_n * input_feature)
 #
-#         # build networks
-#         spatial_kernel_size = A.size(0)
-#         temporal_kernel_size = 9
-#         kernel_size = (temporal_kernel_size, spatial_kernel_size)
-#         in_channels = 15
-#         self.hidden_feature = hidden_feature
+#         self.gcbs = []
+#         for i in range(num_stage):
+#             self.gcbs.append(GC_Block(hidden_feature, p_dropout=p_dropout, node_n=node_n))
 #
-#         spatial_kernel_size = A_d1.size(0)
-#         temporal_kernel_size = 9
-#         kernel_size_d1 = (temporal_kernel_size, spatial_kernel_size)
+#         self.gcbs = nn.ModuleList(self.gcbs)
 #
-#         self.do = nn.Dropout(dropout)
-#         self.act_f = nn.LeakyReLU()
-#         self.gc1 = GraphConvolution(in_channels, hidden_feature, node_n=66)
-#         self.gc2 = GraphConvolution(hidden_feature, hidden_feature, node_n=15)
-#         self.gc3 = GraphConvolution(hidden_feature, hidden_feature, node_n=66)
-#         self.gc4 = GraphConvolution(hidden_feature, in_channels, node_n=66)
+#         self.gc7 = GraphConvolution(hidden_feature, input_feature, node_n=node_n)
+#
+#         self.do = nn.Dropout(p_dropout)
+#         self.act = nn.Tanh()
 #         self.residual = residual
-#
-#         list1 = [[0,1,2,3], [4,5,6,7], [8,9,10,11], [12,13,14,15,16], [17,18,19,20,21]]
-#         self.gd1 = GraphDownSample(hidden_feature, hidden_feature, list1)
-#         self.gu1 = GraphUpSample(hidden_feature, hidden_feature, list1)
-#
-#         list2 = [[0,1,2,3,4]]
-#         self.gd2 = GraphDownSample(hidden_feature, hidden_feature, list2)
-#         self.gu2 = GraphUpSample(hidden_feature, hidden_feature, list2)
-#
-#         self.bn1 = nn.BatchNorm1d(66 * hidden_feature)
-#         self.bn2 = nn.BatchNorm1d(15 * hidden_feature)
-#         self.bn3 = nn.BatchNorm1d(15 * hidden_feature)
-#         self.bn4 = nn.BatchNorm1d(3 * hidden_feature)
-#
-#         self.fcn = nn.Linear(3 * hidden_feature, 2)
 #         self.Softmax = nn.Softmax()
+#         self.fcn = nn.Linear(node_n*input_feature, 2)
 #
 #     def forward(self, x):
 #         y = self.gc1(x)
 #         b, n, f = y.shape
 #         y = self.bn1(y.view(b, -1)).view(b, n, f)
-#         y = self.act_f(y)
-#         y = self.do(y)
-#         batch, n, f = y.shape
-#         y = y.transpose(1,2).reshape(batch, f, 3, 22)
-#
-#         y = self.gd1(y)
-#         b, n, c1, c2 = y.shape
-#         y = self.bn2(y.view(b, -1)).view(b, n, c1, c2)
-#         y = self.act_f(y)
-#         y = self.do(y)
-#         y = y.view(batch, -1, 3*5).transpose(1,2)
-#
-#         y = self.gc2(y)
-#         b, n, f = y.shape
-#         y = self.bn3(y.view(b, -1)).view(b, n, f)
-#         y = self.act_f(y)
-#         y = self.do(y)
-#         y = y.transpose(1,2).reshape(batch, self.hidden_feature, 3, 5)
-#
-#         y = self.gd2(y)
-#         b, n, c1, c2 = y.shape
-#         y = self.bn4(y.view(b, -1))
-#         y = self.act_f(y)
+#         y = self.act(y)
 #         y = self.do(y)
 #
-#         y = self.fcn(y)
-#         y = self.Softmax(y)
+#         for i in range(self.num_stage):
+#             y = self.gcbs[i](y)
 #
+#         if self.residual == True:
+#             y = self.gc7(y)
+#             y = self.fcn(y.view(b, -1))
+#             y = self.Softmax(y)
+#         #else:
+#             # y = self.gc7(y)
+#             # b, n, f = y.shape
+#             # y = self.bn7(y.view(b, -1)).view(b, n, f)
+#             # y = self.act(y)
+#             # y = self.do(y)
 #         return y
+
+@BACKBONES.register_module
+class Discriminator(nn.Module):
+    '''
+    Use GCN as encoder, and then use gcn as a decoder
+    The input is [batch, node_dim, dct_n]   # for example, [16, 66, 15]
+    the out feature of encoder is  [batch, node_dim, feature_len], this is dct feature version.
+    '''
+    def __init__(self,  hidden_feature, layout, strategy, dropout, residual, **kwargs):
+        """
+
+        :param input_feature: num of input feature, dct_n
+        :param hidden_feature: num of hidden feature
+        :param p_dropout: drop out prob.
+        :param num_stage: number of residual blocks
+        :param node_n: number of nodes in graph
+        """
+        super(Discriminator, self).__init__()
+        # load graph
+        self.graph = Graph(layout=layout, strategy=strategy)
+        A = torch.tensor(self.graph.A, dtype=torch.float32, requires_grad=False)
+        self.register_buffer('A', A)
+
+        self.graph_d1 = Graph(layout="h36m_d1", strategy=strategy)
+        A_d1 = torch.tensor(self.graph_d1.A, dtype=torch.float32, requires_grad=False)
+        self.register_buffer('A_d1', A_d1)
+
+        # build networks
+        spatial_kernel_size = A.size(0)
+        temporal_kernel_size = 9
+        kernel_size = (temporal_kernel_size, spatial_kernel_size)
+        in_channels = 15
+        self.hidden_feature = hidden_feature
+
+        spatial_kernel_size = A_d1.size(0)
+        temporal_kernel_size = 9
+        kernel_size_d1 = (temporal_kernel_size, spatial_kernel_size)
+
+        self.do = nn.Dropout(dropout)
+        self.act_f = nn.LeakyReLU()
+        self.gc1 = GraphConvolution(in_channels, hidden_feature, node_n=66)
+        self.gc2 = GraphConvolution(hidden_feature, hidden_feature, node_n=15)
+        self.gc3 = GraphConvolution(hidden_feature, hidden_feature, node_n=66)
+        self.gc4 = GraphConvolution(hidden_feature, in_channels, node_n=66)
+        self.residual = residual
+
+        list1 = [[0,1,2,3], [4,5,6,7], [8,9,10,11], [12,13,14,15,16], [17,18,19,20,21]]
+        self.gd1 = GraphDownSample(hidden_feature, hidden_feature, list1)
+        self.gu1 = GraphUpSample(hidden_feature, hidden_feature, list1)
+
+        list2 = [[0,1,2,3,4]]
+        self.gd2 = GraphDownSample(hidden_feature, hidden_feature, list2)
+        self.gu2 = GraphUpSample(hidden_feature, hidden_feature, list2)
+
+        self.bn1 = nn.BatchNorm1d(66 * hidden_feature)
+        self.bn2 = nn.BatchNorm1d(15 * hidden_feature)
+        self.bn3 = nn.BatchNorm1d(15 * hidden_feature)
+        self.bn4 = nn.BatchNorm1d(3 * hidden_feature)
+
+        self.fcn = nn.Linear(3 * hidden_feature, 2)
+        self.Softmax = nn.Softmax()
+
+    def forward(self, x):
+        y = self.gc1(x)
+        b, n, f = y.shape
+        y = self.bn1(y.view(b, -1)).view(b, n, f)
+        y = self.act_f(y)
+        y = self.do(y)
+        batch, n, f = y.shape
+        y = y.transpose(1,2).reshape(batch, f, 3, 22)
+
+        y = self.gd1(y)
+        b, n, c1, c2 = y.shape
+        y = self.bn2(y.view(b, -1)).view(b, n, c1, c2)
+        y = self.act_f(y)
+        y = self.do(y)
+        y = y.view(batch, -1, 3*5).transpose(1,2)
+
+        y = self.gc2(y)
+        b, n, f = y.shape
+        y = self.bn3(y.view(b, -1)).view(b, n, f)
+        y = self.act_f(y)
+        y = self.do(y)
+        y = y.transpose(1,2).reshape(batch, self.hidden_feature, 3, 5)
+
+        y = self.gd2(y)
+        b, n, c1, c2 = y.shape
+        y = self.bn4(y.view(b, -1))
+        y = self.act_f(y)
+        y = self.do(y)
+
+        y = self.fcn(y)
+        y = self.Softmax(y)
+
+        return y
 
 
 @BACKBONES.register_module
