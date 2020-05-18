f3b9100d14eee8c6c60e81615730c6478ba8dcdf
LittleFlyFish <buliyanran@gmail.com>
2020-05-18 10:36:05 +0800

    "nice"

--- scripts/May13.sh
+++ scripts/May13.sh
@@ -13,7 +13,7 @@ python3 tools/train_hm.py --config ./configs/Dense_GCN_I10_O10_D15_G_hm.py    #H
 python3 tools/train_net_d3p.py --config ./configs/Dense_GCN_I10_O30_D35_G_d3p.py
 python3 tools/train_net_d3p_3d.py --config ./configs/Dense_GCN_I10_O30_D35_G_d3p_3d.py
 
-python3 tools/demo.py --config ./configs/Dense_GCN_I10_O10_D15_G.py
+python3 tools/demo.py --config ./configs/Dense_GCN_I10_O10_D15_G_hm.py
 
 # export PYTHONPATH="/home/yanran/Yanran/Projects/Motion-Cycle-GCN/engineer/utils/":"${PYTHONPATH}"
 
--- tools/demo.py
+++ tools/demo.py
@@ -176,8 +176,8 @@ def main():
 
             _, idct_m = data_utils.get_dct_matrix(seq_len)
             idct_m = Variable(torch.from_numpy(idct_m)).float().cuda(cfg.cuda_num)
-            outputs_t = outputs.view(-1, cfg.data.train.dct_used).transpose(0, 1)
-            outputs_exp = torch.matmul(idct_m[:, 0:cfg.data.train.dct_used], outputs_t).transpose(0, 1).contiguous().view(-1, dim_used_len,
+            outputs_t = outputs.view(-1, cfg.data.train.dct_n).transpose(0, 1)
+            outputs_exp = torch.matmul(idct_m[:, 0:cfg.data.train.dct_n], outputs_t).transpose(0, 1).contiguous().view(-1, dim_used_len,
                                                                                             seq_len).transpose(1, 2)
             pred_expmap = all_seq.clone()
             dim_used = np.array(dim_used)
